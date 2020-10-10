874a1ea5717ccc09c0f62c21adade705aa913007
Helen <helmurphy6@gmail.com>
2020-10-10 20:55:31 +0100

    Updates

---
+++ precache-manifest.5ccde46d6bfae0423714629b71990cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71976edb4b39d4eebbf7b5d2dd7aa4e9",
+    "url": "/movie-watch-list/index.html"
+  },
+  {
+    "revision": "f2343279ed9bc54e7f3f",
+    "url": "/movie-watch-list/static/css/main.dc6419d6.chunk.css"
+  },
+  {
+    "revision": "046ec906b8fe7616fc6b",
+    "url": "/movie-watch-list/static/js/2.8972cb95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie-watch-list/static/js/2.8972cb95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2343279ed9bc54e7f3f",
+    "url": "/movie-watch-list/static/js/main.d7508692.chunk.js"
+  },
+  {
+    "revision": "f098a62447e5e9797c40",
+    "url": "/movie-watch-list/static/js/runtime-main.30436f4c.js"
+  }
+]);
