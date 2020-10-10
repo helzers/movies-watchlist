383e57cb87b9c9817526f1038f9f5288df434ecb
Helen <helmurphy6@gmail.com>
2020-10-10 21:07:38 +0100

    changing Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //Importing packages
 import React, {useCallback, useEffect, useState} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 
 //Importing Styles
 import './App.scss';
